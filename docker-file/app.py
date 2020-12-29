b5556871129a653d421ed2058dca836e92b1faad
WoodProgrammer <emirozbir@kloia.com>
2020-12-30 01:52:17 +0300

    environ example added.

--- docker-file/app.py
+++ docker-file/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    return jsonify({"hello": "NKU"})
+    hello_val = os.environ.get("HELLO_VAL", "default")
+    return jsonify({"hello": "{}".format(hello_val)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
